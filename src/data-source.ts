3a558ea6814fb80c325caba9f3ba4ec2d4d032ed
Ayomide Akinola <drayfocus@gmail.com>
2025-03-03 18:06:33 +0100

    feat: CI/CD setup

--- src/data-source.ts
+++ src/data-source.ts
@@ -8,6 +8,8 @@ import { Transaction } from "./models/Transaction";
 import { UserBank } from "./models/UserBank";
 import { Wallet } from "./models/Wallet";
 import pkg from "typeorm";
+import fs from "fs";
+
 const { DataSource } = pkg;
 
 export const AppDataSource = new DataSource({
@@ -15,11 +17,17 @@ export const AppDataSource = new DataSource({
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT || "5432"),
   username: process.env.DB_USERNAME,
-
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   synchronize: true,
   logging: true,
+  ssl:
+    process.env.DB_USE_SSL === "true"
+      ? {
+          rejectUnauthorized: true,
+          ca: fs.readFileSync(__dirname + "/database/ca-cert.crt").toString(),
+        }
+      : false,
   entities: [PointTransaction, Transaction, UserBank, Wallet],
   subscribers: [],
   migrations: ["src/database/migrations/*.ts"],
